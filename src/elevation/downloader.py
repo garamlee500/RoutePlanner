f2c903f2cfa66f88e1be02b89bd72e030834ebc8
Garam Lee <garamlee2005@gmail.com>
2023-07-18 14:28:25 +0100

    Formatted downloader.py

--- src/elevation/downloader.py
+++ src/elevation/downloader.py
@@ -1,15 +1,12 @@
-"""
-Automatically downloads and parses elevation data for a given set of nodes
-"""
-
-from typing import List, Tuple, Set
+from typing import List, Tuple
 from math import floor, ceil
+from io import BytesIO
 import zipfile
 import requests
-from io import BytesIO
 from distance_formulas import haversine_node_distance
 
-def _generate_geotiff_filename_for_tile(lat_lon: Tuple[int, int]):
+
+def _generate_tile_identifier(lat_lon: Tuple[int, int]):
     if lat_lon[0] >= 0:
         lat_coord = "N" + str(lat_lon[0]).rjust(2, '0')
     else:
@@ -20,51 +17,41 @@ def _generate_geotiff_filename_for_tile(lat_lon: Tuple[int, int]):
     else:
         lon_coord = "W" + str(-lat_lon[1]).rjust(3, '0')
 
-    return f"ASTGTMV003_{lat_coord}{lon_coord}_dem.tif"
+    return lat_coord + lon_coord
+
+
+def _generate_geotiff_filename_for_tile(lat_lon: Tuple[int, int]):
+    return f"ASTGTMV003_{_generate_tile_identifier(lat_lon)}_dem.tif"
 
 
 def _generate_subzip_filename_for_tile(lat_lon: Tuple[int, int]):
-    if lat_lon[0] >= 0:
-        lat_coord = "N" + str(lat_lon[0]).rjust(2, '0')
-    else:
-        lat_coord = "S" + str(-lat_lon[0]).rjust(2, '0')
-
-    if lat_lon[1] >= 0:
-        lon_coord = "E" + str(lat_lon[1]).rjust(3, '0')
-    else:
-        lon_coord = "W" + str(-lat_lon[1]).rjust(3, '0')
-
-    return f"ASTGTMV003_{lat_coord}{lon_coord}.zip"
+    return f"ASTGTMV003_{_generate_tile_identifier(lat_lon)}.zip"
 
 
 def _generate_zip_filename_for_tile(lat_lon: Tuple[int, int]):
-    if lat_lon[0] >= 0:
-        lat_coord = "N" + str(lat_lon[0]).rjust(2, '0')
-    else:
-        lat_coord = "S" + str(-lat_lon[0]).rjust(2, '0')
-
-    if lat_lon[1] >= 0:
-        lon_coord = "E" + str(lat_lon[1]).rjust(3, '0')
-    else:
-        lon_coord = "W" + str(-lat_lon[1]).rjust(3, '0')
-
-    return f"Download_{lat_coord}{lon_coord}.zip"
+    return f"Download_{_generate_tile_identifier(lat_lon)}.zip"
 
 
-def get_elevation_for_nodes(nodes: List[Tuple[int, float, float]],
-                            aster_gdem_api_endpoint: str = "https://gdemdl.aster.jspacesystems.or.jp/download/") -> \
-List[float]:
+def get_elevation_for_nodes(nodes: List[Tuple[int, float, float]], aster_gdem_api_endpoint) -> List[float]:
+    """
+    Gets the elevation of all nodes in a given list of nodes
+    See https://www.jspacesystems.or.jp/ersdac/GDEM/E/2.html for format of geotiff files
+    :param nodes:
+    :param aster_gdem_api_endpoint:
+    :return:
+    """
 
     # Moved import into only submodule that requires it due to large loading times
     # on start up of main.py despite not needing it most of the time
     import rasterio
 
-
+    # Use a simple list to store all tiles - unlikely to reach beyond length 4
     required_tiles: List[Tuple[int, int]] = []
     tile_data = []
 
     for node in nodes:
-        # coordinate lat, lon is stored in tile floor(lat), floor(lon) (considering negatives as south and west)
+        # Detects the 4 pixels of which to sample elevation from
+        # and then the tiles these pixels are on
         tiles = [(floor(node[1]), floor(node[2])),
                  (floor(node[1] + 1 / 3600), floor(node[2])),
                  (floor(node[1]), floor(node[2] + 1 / 3600)),
@@ -76,6 +63,7 @@ List[float]:
 
     for tile in required_tiles:
         try:
+            # Server provides data in this format - not much we can do to make this cleaner
             response = requests.get(aster_gdem_api_endpoint + _generate_zip_filename_for_tile(tile))
             with zipfile.ZipFile(BytesIO(response.content)) as download_zip:
                 with download_zip.open(_generate_subzip_filename_for_tile(tile)) as sub_download_zip_file:
@@ -90,28 +78,34 @@ List[float]:
 
     elevations = []
     for node in nodes:
-
         surrounding_elevations = []
         surrounding_distances = []
-
         elevation_collection_points = [(node[1], node[2]),
                                        (node[1] + 1 / 3600, node[2]),
                                        (node[1], node[2] + 1 / 3600),
                                        (node[1] + 1 / 3600, node[2] + 1 / 3600)
                                        ]
 
+        # Considering the centre of each pixel of the (combined) geotiff file as a grid point
+        # This samples the 4 'corner' elevations for each node, finding the elevation, and distance
+        # from the node. We then use inverse distance weighted average to approximate elevation of
+        # node itself
         for elevation_point in elevation_collection_points:
             tile = (floor(elevation_point[0]), floor(elevation_point[1]))
             tile_elevations = tile_data[required_tiles.index(tile)]
             lat_index = ceil(elevation_point[0])*3600 - floor(elevation_point[0] * 3600)
             lon_index = floor(elevation_point[1] * 3600) - floor(elevation_point[1]) * 3600
 
-            surrounding_distances.append(haversine_node_distance(floor(elevation_point[0] * 3600)/3600,
-                                                                 floor(elevation_point[1] * 3600)/3600,
-                                                                 node[1],
-                                                                 node[2]
-                                                                 ))
-            # Prevents zero division errors later
+            surrounding_distances.append(
+                haversine_node_distance(
+                    floor(elevation_point[0] * 3600)/3600,
+                    floor(elevation_point[1] * 3600)/3600,
+                    node[1],
+                    node[2]
+                )
+            )
+
+            # Prevents zero division errors during inverse distance weighting average
             surrounding_distances[-1] = max(surrounding_distances[-1], 1e-50)
 
             try:
@@ -126,7 +120,7 @@ List[float]:
             weighted_elevation_sum += surrounding_elevations[i]/surrounding_distances[i]
             inverse_distance_sum += 1/surrounding_distances[i]
 
-        # Finds the inverse distance weighted average of elvation of 4 surrounding grid points
+        # Finds the inverse distance weighted average of elevation of 4 surrounding grid points
         # Not extremely accurate, but generally ok - grid squares are only about 30mx30m
         # Additionally interpolation is mainly to smooth out points rather than to make accurate predictions
         # This prevents 'vertical wall' edges which are really short but have huge elevation gain
@@ -134,7 +128,7 @@ List[float]:
         # untraversable edge leading to traversal time measured in hours/days
         # The other alternative would be to just set some min speed to Tobler's hiking function, but this would
         # break the convexity of the inverse of Tobler's hiking function, which would mean a star would no longer be
-        # provable optimal (although I doubt the effect would be serious)
+        # provably optimal (although I doubt the effect would be serious)
         elevations.append(weighted_elevation_sum/inverse_distance_sum)
 
     return elevations
