9333743178f487dd5572985c17b7d05772c3d9a8
Garam Lee <garamlee2005@gmail.com>
2023-09-03 20:29:37 +0100

    Updated speed test

--- src/speed_test.py
+++ src/speed_test.py
@@ -2,14 +2,16 @@ from load_data import load_node_list
 import graph_algorithms
 from timeit import timeit
 
-nodes = load_node_list(include_id=True)
+THREAD_COUNT = 1
 
-print(timeit(lambda: graph_algorithms.compute_2D_nearest_neighbours(nodes,
-                                                                    grid_filename="map_data/grid2d.csv",
-                                                                    grid_distance=10,
-                                                                    threads=300),
-             number=1)
-      )
+nodes = load_node_list(include_id=True)
+finish_time = timeit(lambda: graph_algorithms.compute_2D_nearest_neighbours(nodes,
+                                                                	grid_filename="map_data/grid2d.csv",
+                                                                	grid_distance=10,
+                                                                	threads=THREAD_COUNT),
+                     	number=1)
+
+print(f"Execution took {finish_time}s with {THREAD_COUNT} threads")
 #
 # a = graph_algorithms.MapGraphInstance()
 # a.isoline(0, 1000)
